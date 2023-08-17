218eab1282959afa3dfb1c558d228dd58c02a575
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-17 21:38:53 +0000

    added jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,30 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+import os
+#go to jwt extended, documentation, and basic usage to get the imports and the pipenv install
+
 
 api = Blueprint('api', __name__)
+
+#this comes fro m the jwt website
+@app.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
